e6aa57cf81a7bbf3773007759ee633a80c48cc27
Marcio Ribeiro <binary@b1n.org>
2015-08-01 20:52:48 -0300

    [update] Running pgbadger and returning output as json

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"os/exec"
 	"time"
 )
 
 const SLEEP_TIME = 5 * time.Second
 const AWS_LOG_PREFIX = "%t:%r:%u@%d:[%p]:"
 const PG_BADGER_CMD = "/usr/local/bin/pgbadger"
+const OUTPUT_JSON_FILE_PREFIX = "/data/output"
+const INC_CTRL_FILE = "/data/inc_ctrl.dat"
 
 func sleep() {
 	time.Sleep(SLEEP_TIME)
@@ -27,7 +30,23 @@ func fetchLogLines(limit, offset int) []string {
 	return []string{"raw1", "raw2"}
 }
 
-func processLogLines(lines []string) []string {
+func analyzeLogLines(lines []string) []string {
+	now := time.Now()
+	output_file := fmt.Sprintf("%s-%d%d%d.json",
+	    OUTPUT_JSON_FILE_PREFIX, now.Year(), now.Month(), now.Day())
+
+	cmd := exec.Command(PG_BADGER_CMD,
+	  "--prefix", AWS_LOG_PREFIX,
+		"--last-parsed", INC_CTRL_FILE,
+		"--output", output_file,
+		"-",
+  )
+	stdin, _ := cmd.StdinPipe()
+	defer stdin.Close()
+	for _, line := range lines {
+		stdin.Write([]byte(line))
+	}
+	fmt.Println(cmd.Stdout)
 	return []string{"proc1", "proc2"}
 }
 
@@ -37,7 +56,7 @@ func saveLogLines(data []string) int {
 
 func work() {
 	lines := fetchLogLines(10, 20)
-	data := processLogLines(lines)
+	data := analyzeLogLines(lines)
 	saved := saveLogLines(data)
 	if saved > 0 {
 		info(fmt.Sprintf("Saved %d lines...", saved))
