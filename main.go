5433fa69914a58848a9970a0111e95826e5d5d03
Marcio Ribeiro <binary@b1n.org>
2015-08-03 01:13:58 -0300

    [update] Broke main down to several files to better organize. This is starting to get big... gee

--- main.go
+++ main.go
@@ -1,105 +1,71 @@
 package main
 
 import (
-	"bytes"
-	//	"encoding/json"
+	"code.google.com/p/gcfg"
 	"fmt"
+	"github.com/Sirupsen/logrus"
 	"github.com/marpaia/graphite-golang"
-	"log"
-	"os/exec"
-	"strings"
 	"time"
 )
 
 const (
-	SLEEP_TIME     = 2 * time.Second
-	RDS_LOG_PREFIX = "%t:%r:%u@%d:[%p]:"
-	PGBADGER_CMD   = "/usr/local/bin/pgbadger"
-	INC_CTRL_FILE  = "/data/inc_ctrl.dat"
-	GRAPHITE_HOST  = "127.0.0.1"
-	GRAPHITE_PORT  = 9999
+	sleepTime  = 2 * time.Second
+	configFile = "/etc/pglogger.conf"
 )
 
-type LogMinute struct {
-	Timestamp   time.Time
-	Connections int
-	Sessions    int
-	Selects     int
-	Inserts     int
-	Updates     int
-	Max         float64
-	Min         float64
-	Duration    float64
+var log *logrus.Logger
+var config Config
+
+type Config struct {
+	AwsCredentials struct {
+		Key       string
+		SecretKey string
+	}
+	Graphite struct {
+		Host string
+		Port int
+	}
 }
 
 func sleep() {
-	time.Sleep(SLEEP_TIME)
-}
-
-func info(msg string) {
-	var buf bytes.Buffer
-	logger := log.New(&buf, "", log.LstdFlags)
-	logger.Print(msg)
-	fmt.Print(&buf)
-}
-
-func fetchLogLines(limit, offset int) []string {
-	info("Fetching log portion...")
-	return []string{"raw1", "raw2"}
-}
-
-func analyzeLogLines(lines []string) []string {
-	cmd := exec.Command(PGBADGER_CMD,
-		"--prefix", RDS_LOG_PREFIX,
-		"--last-parsed", INC_CTRL_FILE,
-		"--outfile", "-",
-		"--extension", "json",
-		"--format", "stderr",
-		"-",
-	)
-
-	// Handling Stdin
-	cmd.Stdin = strings.NewReader(strings.Join(lines, "\n"))
-
-	// Handling Stdout
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(out.String())
-
-	return []string{"proc1", "proc2"}
-}
-
-func saveLogLines(data []string) int {
-	return 10
+	time.Sleep(sleepTime)
 }
 
 func work() {
-	lines := fetchLogLines(10, 20)
-	data := analyzeLogLines(lines)
-	saved := saveLogLines(data)
-	if saved > 0 {
-		info(fmt.Sprintf("Saved %d lines...", saved))
-	}
+	SendLogs(AnalyseLogs(FetchLogs()))
 }
 
 func loadGraphite() {
-	Graphite, err := graphite.NewGraphite(GRAPHITE_HOST, GRAPHITE_PORT)
+	host := config.Graphite.Host
+	port := config.Graphite.Port
+	Graphite, err := graphite.NewGraphite(host, port)
 	if err != nil {
-		Graphite = graphite.NewGraphiteNop(GRAPHITE_HOST, GRAPHITE_PORT)
+		Graphite = graphite.NewGraphiteNop(host, port)
 	}
-	info(fmt.Sprintf("Graphite conn: %#v", Graphite))
+	log.Info(fmt.Sprintf("Graphite conn: %#v", Graphite))
+}
+
+func loadLogging() {
+	// XXX (mmr) : find a better way to do this
+	log = logrus.New()
+	fmt := new(logrus.TextFormatter)
+	fmt.FullTimestamp = true
+	log.Formatter = fmt
+	log.Level = logrus.DebugLevel
+}
+
+func loadConfig() {
+	gcfg.ReadFileInto(&config, configFile)
 }
 
 func init() {
+	loadLogging()
+	loadConfig()
 	loadGraphite()
 }
 
 func main() {
-	info("Firing up...")
+	log.Info("Firing up...")
 	for {
 		work()
 		sleep()
